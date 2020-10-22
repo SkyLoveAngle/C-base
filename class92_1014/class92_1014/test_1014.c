71df62e9f1fc80a63f3cc862e43f7139f0f3c506
Qin-SKY <1920909528@qq.com>
2020-10-22 14:23:46 +0800

    10.14 class operation

---
+++ class92_1014/class92_1014/test_1014.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<windows.h>
+
+int main() {
+	//写个代码实现n的阶乘
+	//int n = 5;
+	//int result = 1;
+	//for (int i = 1; i <= 5; i++) {
+	//	result = result*i;
+	//}
+	//printf("result = %d\n",result);
+
+	 //计算 1!+2!+3!+……+10!
+	//int n = 10;
+	//int sum = 0;
+	//for (int cur = 1; cur <= n;cur++) {
+	//	int factory = 1;
+	//	for (int i = 1; i <= cur;i++ ) {
+	//		factory = factory*i;
+	//	}
+	//	sum = sum + factory;
+	//}
+
+	//编写代码，演示多个字符从两端移动，向中间汇聚
+
+
+	system("pause");
+	return 0;
+}
