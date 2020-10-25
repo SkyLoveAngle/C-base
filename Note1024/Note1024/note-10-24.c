cd36bdc96c562d958f81bb1d6c0b056f7545b06e
Qin-SKY <1920909528@qq.com>
2020-10-25 21:11:53 +0800

    C语言函数基础

---
+++ Note1024/Note1024/note-10-24.c
@@ -0,0 +1,62 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+//调用函数比较两个数的大小
+//int get_Max(x, y) {
+//	if (x>y) {
+//		return x;
+//	}
+//	return y;
+//}
+//
+//
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	int ret = get_Max(a,b);
+//	printf("%d",ret);
+//system("pause");
+//return 0;
+//}
+
+////写一个函数可以交换两个变量的数值
+////
+//void swap1(int num1,int num2) {
+//	int tmp = 1;
+//	tmp = num1;
+//	num1 = num2;
+//	num2 = tmp;
+//}
+////
+//void swap2(int* num1, int* num2) {
+//	int tmp = 1;
+//	tmp = *num1;
+//	*num1 = *num2;
+//	*num2 = tmp;
+//}
+//int main(){
+//	int num1 = 10;
+//	int num2 = 20;
+//	swap1(num1,num2);
+//	printf("函数swap1调用后的结果为:num1=%d,num2=%d\n",num1,num2);
+//	swap2(&num1, &num2);
+//	printf("函数swap2调用后的结果为:num1=%d,num2=%d\n", num1, num2);
+//	system("pause");
+//	return 0;
+//}
+
+//1. 写一个函数可以判断一个数是不是素数。
+
+
+int main() {
+	int x = 10;
+	is
+	system("pause");
+	return 0;
+}
+
+//2. 写一个函数判断一年是不是闰年。
+
+//3. 写一个函数，实现一个整形有序数组的二分查找。
+
+//4. 写一个函数，每调用一次这个函数，就会将num的值增加1。
